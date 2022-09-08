3afb10c2843db1b42fbb810fa38e86e14fcfe427
Paweł Łęczycki <pawel@msd.info.pl>
2022-09-08 11:08:01 +0200

    DataContract

--- Models/User.cs
+++ Models/User.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace Models
 {
+    [DataContract]
     public class User : Entity
     {
+        [DataMember]
         public string UserName { get; set; }
         public string Password { get; set; }
     }
