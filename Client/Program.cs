465113c59e88c869a43d5c3e55fe454fec30cfe9
Paweł Łęczycki <pawel@msd.info.pl>
2022-09-09 11:13:38 +0200

    SignalR

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,5 +1,6 @@
 ﻿
 using Client;
+using Microsoft.AspNetCore.SignalR.Client;
 using Models;
 using Newtonsoft.Json;
 using System.Net.Http.Headers;
@@ -7,64 +8,107 @@ using System.Net.Http.Json;
 
 
 
+var signalR = new HubConnectionBuilder()
+                .WithUrl("http://localhost:5299/signalR/Demo")
+                .Build();
+
+signalR.On<string>(nameof(HelloResponse), x => HelloResponse(x));
+signalR.On<string>("Echo", x => Console.WriteLine(x));
+
+signalR.On("Welcome", ConsoleWrite());
+signalR.On("NewConnection", ConsoleWrite());
+signalR.On("SendMessage", ConsoleWrite());
+
+await signalR.StartAsync();
+
+
+await signalR.SendAsync("SayHello");
+await signalR.SendAsync("SayHelloToMe", "Paul");
+
+if(DateTime.Now.Second % 2 == 0)
+    await signalR.SendAsync("JoinGroup", "myFavouriteGroup");
 
 
 
-var handler = new HttpClientHandler()
+while (true)
 {
-    AutomaticDecompression = System.Net.DecompressionMethods.Brotli
-};
-
-var httpClient = new HttpClient(handler);
-
-httpClient.BaseAddress = new Uri("https://localhost:7013/api/");
+    var connectionId = Console.ReadLine();
+    await signalR.SendAsync("Echo", connectionId);
+    await signalR.SendAsync("SendMessage", connectionId, Console.ReadLine());
+}
 
 
-httpClient.DefaultRequestHeaders.Accept.Add(MediaTypeWithQualityHeaderValue.Parse("application/json"));
-httpClient.DefaultRequestHeaders.AcceptEncoding.Add(new StringWithQualityHeaderValue("br"));
+Console.ReadLine();
+
+void HelloResponse(string message)
+{
+    Console.WriteLine(message);
+}
 
 
-var response = await httpClient.GetAsync("Parents");
-/*if (response.StatusCode != System.Net.HttpStatusCode.OK)
-    return;*/
-/*if (!response.IsSuccessStatusCode)
-    return;*/
-response.EnsureSuccessStatusCode();
-//var parent = await response.Content.ReadFromJsonAsync<Parent>();
-//Console.WriteLine(JsonConvert.SerializeObject(parent));
-Console.WriteLine(await response.Content.ReadAsStringAsync());
+static async Task WebAPI()
+{
+    var handler = new HttpClientHandler()
+    {
+        AutomaticDecompression = System.Net.DecompressionMethods.Brotli
+    };
+
+    var httpClient = new HttpClient(handler);
+
+    httpClient.BaseAddress = new Uri("https://localhost:7013/api/");
 
 
-response = await httpClient.PostAsJsonAsync("Users/login", new Credentials { UserName = "admin", Password = "nimda" });
-response.EnsureSuccessStatusCode();
-
-var token = await response.Content.ReadAsStringAsync();
-
-Console.WriteLine(token);
-
-httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token.Trim('"'));
+    httpClient.DefaultRequestHeaders.Accept.Add(MediaTypeWithQualityHeaderValue.Parse("application/json"));
+    httpClient.DefaultRequestHeaders.AcceptEncoding.Add(new StringWithQualityHeaderValue("br"));
 
 
-response = await httpClient.GetAsync("Users");
-response.EnsureSuccessStatusCode();
-
-Console.WriteLine(await response.Content.ReadAsStringAsync());
-
-response = await httpClient.DeleteAsync("Users/1");
-response.EnsureSuccessStatusCode();
-
-
-response = await httpClient.GetAsync("Users");
-response.EnsureSuccessStatusCode();
-
-Console.WriteLine(await response.Content.ReadAsStringAsync());
+    var response = await httpClient.GetAsync("Parents");
+    /*if (response.StatusCode != System.Net.HttpStatusCode.OK)
+        return;*/
+    /*if (!response.IsSuccessStatusCode)
+        return;*/
+    response.EnsureSuccessStatusCode();
+    //var parent = await response.Content.ReadFromJsonAsync<Parent>();
+    //Console.WriteLine(JsonConvert.SerializeObject(parent));
+    Console.WriteLine(await response.Content.ReadAsStringAsync());
 
 
+    response = await httpClient.PostAsJsonAsync("Users/login", new Credentials { UserName = "admin", Password = "nimda" });
+    response.EnsureSuccessStatusCode();
 
-var openApiHttpClient = new HttpClient();
-openApiHttpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token.Trim('"'));
-var openApiClient = new openapiClient("https://localhost:7013", openApiHttpClient);
+    var token = await response.Content.ReadAsStringAsync();
 
-var shoppingLists = await openApiClient.ShoppingListsAllAsync();
+    Console.WriteLine(token);
 
-Console.WriteLine();
+    httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token.Trim('"'));
+
+
+    response = await httpClient.GetAsync("Users");
+    response.EnsureSuccessStatusCode();
+
+    Console.WriteLine(await response.Content.ReadAsStringAsync());
+
+    response = await httpClient.DeleteAsync("Users/1");
+    response.EnsureSuccessStatusCode();
+
+
+    response = await httpClient.GetAsync("Users");
+    response.EnsureSuccessStatusCode();
+
+    Console.WriteLine(await response.Content.ReadAsStringAsync());
+
+
+
+    var openApiHttpClient = new HttpClient();
+    openApiHttpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token.Trim('"'));
+    var openApiClient = new openapiClient("https://localhost:7013", openApiHttpClient);
+
+    var shoppingLists = await openApiClient.ShoppingListsAllAsync();
+
+    Console.WriteLine();
+}
+
+static Action<string> ConsoleWrite()
+{
+    return x => Console.WriteLine(x);
+}
---
+++ SignalR/Hubs/DemoHub.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Xml.Linq;
+
+namespace SignalR.Hubs
+{
+    public class DemoHub : Hub
+    {
+        public override async Task OnConnectedAsync()
+        {
+            Console.WriteLine(Context.ConnectionId);
+
+            await Clients.Caller.SendAsync("Welcome", "Welcome in RignalR");
+            await Clients.Others.SendAsync("NewConnection", Context.ConnectionId);
+
+            await base.OnConnectedAsync();
+        }
+
+
+        public async Task SayHello()
+        {
+            await Clients.Caller.SendAsync("HelloResponse", "Hello");
+        }
+
+        public async Task SayHelloToMe(string myName)
+        {
+            await Clients.Caller.SendAsync("HelloResponse", $"Hello {myName}");
+        }
+
+        public async Task Echo(string message)
+        {
+            await Clients.Caller.SendAsync("Echo", message);
+        }
+
+        public async Task SendMessage(string connectionId, string message)
+        {
+            await Clients.Client(connectionId).SendAsync(nameof(SendMessage), message);
+        }
+
+
+        public async Task JoinGroup(string groupName)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+
+            await Clients.Group(groupName).SendAsync("Welcome", $"New member in group {groupName}");
+        }
+    }
+}
