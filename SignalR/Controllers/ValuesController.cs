585ed9e5a8c5b0253c821bff18423d9bd027a472
Paweł Łęczycki <pawel@msd.info.pl>
2022-09-09 11:37:03 +0200

    API + Hub

---
+++ SignalR/Controllers/ValuesController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using SignalR.Hubs;
+using System.Runtime.CompilerServices;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SignalR.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private IHubContext<DemoHub> _hubContext;
+
+        public ValuesController(IHubContext<DemoHub> hubContext)
+        {
+            _hubContext = hubContext;
+        }
+
+
+
+
+
+        // GET: api/<ValuesController>
+        [HttpGet]
+        public async Task<IEnumerable<string>> Get()
+        {
+            await _hubContext.Clients.All.SendAsync("Echo", "GET: /api/Values/");
+
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<ValuesController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<ValuesController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ValuesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ValuesController>/5
+        [HttpDelete("{id}")]
+        public async Task Delete(int id)
+        {
+            await _hubContext.Clients.All.SendAsync("Echo", $"DELETE: /api/Values/{id}");
+        }
+    }
+}
--- SignalR/Program.cs
+++ SignalR/Program.cs
@@ -2,11 +2,14 @@ using SignalR.Hubs;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddControllers();
 builder.Services.AddSignalR();
 
 var app = builder.Build();
 
+
 app.MapHub<DemoHub>("signalR/demo");
 
 
+app.MapControllers();
 app.Run();
